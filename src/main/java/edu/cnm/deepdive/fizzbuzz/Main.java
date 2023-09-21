2b6cce222dba88b3b865b4923ffe7f8c806b051b
drewsbrews <143551649+drewsbrews@users.noreply.github.com>
2023-09-21 15:00:59 -0600

    using improved switch statement

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -5,24 +5,22 @@ public class Main {
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
       int remainder = i % 15;
+      String value;
       switch(remainder) {
         case 0:
-          System.out.println("fizzbuzz");
+          value = "fizzbuzz";
           break;
-        case 3:
-        case 6:
-        case 9:
-          case 12:
+        case 3, 6, 9, 12:
 
-          System.out.println("fizz");
+          value = "fizz";
           break;
-        case 5:
-          case 10:
-          System.out.println("buzz");
+        case 5, 10:
+          value = "buzz";
           break;
         default:
-          System.out.println(i);
+          value = String.valueOf(i);
       }
+      System.out.println(value);
     }
   }
 }
