9a441a3f1e8b83d6052083589ff3d56b0f927f51
drewsbrews <143551649+drewsbrews@users.noreply.github.com>
2023-09-21 14:50:22 -0600

    classic switch based solution

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -4,19 +4,25 @@ public class Main {
 
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
-      boolean divisibleBy3 = (i % 3 == 0);
-      boolean divisibleBy5 = (i % 5 == 0);
-      if (divisibleBy3 || divisibleBy5) {
-        if (divisibleBy3); {
-        }
-        System.out.println("fizz");
+      int remainder = i % 15;
+      switch(remainder) {
+        case 0:
+          System.out.println("fizzbuzz");
+          break;
+        case 3:
+        case 6:
+        case 9:
+          case 12:
+
+          System.out.println("fizz");
+          break;
+        case 5:
+          case 10:
+          System.out.println("buzz");
+          break;
+        default:
+          System.out.println(i);
       }
-      if (divisibleBy5) {
-        System.out.println("buzz");
-      } else {
-        System.out.println(i);
-      }
-      System.out.println();
     }
   }
 }
